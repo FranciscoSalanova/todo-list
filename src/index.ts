be8142b286ecadd801b27daf24b2dd9540052ffb
FranciscoSalanova <fsalanova.16@gmail.com>
2022-09-29 12:25:12 -0300

    Completing basic functionality

--- src/index.ts
+++ src/index.ts
@@ -10,22 +10,52 @@ type Task = {
 const list = document.querySelector<HTMLUListElement>('#list');
 const form = document.querySelector<HTMLFormElement>('#new-task-form');
 const input = document.querySelector<HTMLInputElement>('#new-task-title');
+const tasks: Task[] = loadTasks();
+
+tasks.forEach(task => {
+  addListItem(task);
+})
 
 form?.addEventListener('submit', e => {
   e.preventDefault();
   if (input?.value == '' || input?.value == null) {
     return;
   }
-  const newTask = {
+  const newTask: Task = {
     id: uuidV4(),
     title: input.value,
     completed: false,
     createdAt: new Date()
   }
+  tasks.push(newTask);
+  saveTasks();
 
   addListItem(newTask);
+  input.value = '';
 });
 
 function addListItem(task: Task) {
-  // TODO
+  const item = document.createElement('li');
+  const label = document.createElement('label');
+  const checkbox = document.createElement('input');
+  checkbox.addEventListener('change', () => {
+    task.completed = checkbox.checked;
+  })
+  checkbox.type = 'checkbox';
+  checkbox.checked = task.completed;
+  label.append(checkbox, task.title);
+  item.append(label);
+  list?.append(item);
+}
+
+function saveTasks() {
+  localStorage.setItem('TASKS', JSON.stringify(tasks));
+}
+
+function loadTasks(): Task[] {
+  const taskJSON = localStorage.getItem('TASKS');
+  if (taskJSON == null) {
+    return [];
+  }
+  return JSON.parse(taskJSON);
 }
